c6b5a9c832d449398021b120aab981051f23024a
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-09-27 22:20:03 -0300

    Select / input text - Recuperando e atribuindo valores

--- 6 - Recuperando valores de input e selects/script.js
+++ 6 - Recuperando valores de input e selects/script.js
@@ -3,3 +3,22 @@ var j = jQuery.noConflict();
 j(document).ready(() => {
     
 })
+
+/* j("#nome").val
+? (n){var r,e,i,t=this[0];return arguments.length?(i=m(n),this.each(function(e){var t;1===this.nodeType&&(null==(t=i?n.call(this,e,S(this).val()):n)?t="":"number"==typeof t?t+="":Array.isArray(t)&&(t=S?
+j("#nome").val()
+'Dhionathan'
+j("#origem").val()
+'2'
+j("#origem").val()
+'4'
+j("#origem").val('Dhionathan')
+S.fn.init [select#origem]
+j("#nome").val('DHionathan')
+S.fn.init [input#nome]
+j("#nome").val('Dhionathan')
+S.fn.init [input#nome]
+j("#origem").val('3')
+S.fn.init [select#origem]
+j("#origem").val('2')
+S.fn.init [select#origem] */
